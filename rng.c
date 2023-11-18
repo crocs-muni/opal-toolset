69979dbf378052b3e9a8156c9fe40ff4d5a0fba7
Tamara Čierniková <tam.cier@gmail.com>
2023-11-18 17:09:44 +0100

    Replace user-given required repeats by chunk (buffer) size + polish

--- rng.c
+++ rng.c
@@ -5,13 +5,11 @@
 
 #include <errno.h>
 
-#define BUFFER_SIZE 512
-
 enum arg_keys
 {
     ARG_KEY_DEVICE_FILE = 'd',
     ARG_KEY_REQ_BYTES = 'b',
-    ARG_KEY_REQ_REPEATS = 'r',
+    ARG_KEY_CHUNK_SIZE = 'c',
     ARG_KEY_OUTPUT_FILE = 'o',
     ARG_KEY_LOG_LEVEL = 'l',
     ARG_KEY_USE_SCSI = 's',
@@ -20,12 +18,12 @@ enum arg_keys
 
 static struct argp_option options[] = {
     { "device", ARG_KEY_DEVICE_FILE, "device", 0, "Disk device", 0 },
-    { "bytes", ARG_KEY_REQ_BYTES, "number of bytes", 0, "Number of random bytes within a sequence", 0 },
-    { "repeats", ARG_KEY_REQ_REPEATS, "number of repeats", 0, "Number of random byte sequences to acquire", 0 },
-    { "output", ARG_KEY_OUTPUT_FILE, "file", 0, "Output file", 0 },
+    { "bytes", ARG_KEY_REQ_BYTES, "number of bytes", 0, "Number of random bytes within a sequence (defaults to 32 bytes)", 0 },
+    { "chunk-size", ARG_KEY_CHUNK_SIZE, "size of chunks in bytes", 0, "Divide acquisition of the random sequence into chunks of a specified size (defaults to 512 bytes)", 0 },
+    { "output", ARG_KEY_OUTPUT_FILE, "file", 0, "Output file (defaults to stdout)", 0 },
     { "log-level", ARG_KEY_LOG_LEVEL, "level", 0, "Log level", 0 },
     { "scsi", ARG_KEY_USE_SCSI, 0, 0, "Use SCSI", 0 },
-    { "hex", ARG_KEY_HEX_OUTPUT, 0, 0, "Output random bytes in hexadecimal", 0 },
+    { "hex", ARG_KEY_HEX_OUTPUT, 0, 0, "Output random bytes in hexadecimal (NOT IMPLEMENTED)", 0 },
     { 0 }
 };
 
@@ -34,14 +32,14 @@ struct arguments
     char *dev_file;
     char *out_file_name;
     size_t req_bytes;
-    size_t req_repeats;
+    size_t chunk_size;
     bool use_scsi_sec;
     bool hex_output;
 } args = {
     .dev_file = NULL,
     .out_file_name = NULL,
     .req_bytes = 32,
-    .req_repeats = 1,
+    .chunk_size = 512,
     .use_scsi_sec = false,
     .hex_output = false,
 };
@@ -93,8 +91,8 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
             return 1;
         }
         break;
-    case ARG_KEY_REQ_REPEATS:
-        if (parse_num(arg, &args->req_repeats)) {
+    case ARG_KEY_CHUNK_SIZE:
+        if (parse_num(arg, &args->chunk_size)) {
             return 1;
         }
         break;
@@ -110,11 +108,11 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
         break;
     case ARG_KEY_USE_SCSI:
         args->use_scsi_sec = true;
-	break;
+        break;
     case ARG_KEY_HEX_OUTPUT:
         // args->hex_output = true;
-	fprintf(stderr, "Option -%c not implemented.\n", ARG_KEY_HEX_OUTPUT);
-	print_usage("rng");
+        fprintf(stderr, "Option -%c not implemented.\n", ARG_KEY_HEX_OUTPUT);
+        print_usage("rng");
     default:
         return ARGP_ERR_UNKNOWN;
     }
@@ -135,7 +133,7 @@ int main(int argc, char **argv)
         goto exit;
     }
 
-    if (!(buffer = malloc(BUFFER_SIZE))) {
+    if (!(buffer = malloc(args.chunk_size))) {
         LOG(ERROR, "Out of memory.\n");
         err = 1;
         goto exit;
@@ -155,28 +153,30 @@ int main(int argc, char **argv)
         goto disk_cleanup;
     }
 
-    for (int req_i = 0; req_i < args.req_repeats; ++req_i) {
-        size_t bytes_read = 0;
+    size_t bytes_read = 0;
 
-        while (bytes_read < args.req_bytes) {
-            memset(buffer, 0, BUFFER_SIZE);
-            size_t current_req_bytes = min(args.req_bytes - bytes_read, BUFFER_SIZE);
+    while (bytes_read < args.req_bytes) {
+        memset(buffer, 0, args.chunk_size);
+        size_t current_req_bytes = min(args.req_bytes - bytes_read, args.chunk_size);
 
-            if ((err = get_random(&dev, buffer, current_req_bytes))) {
-                LOG(ERROR, "Failed to get random data.\n");
-                sleep(5);
-                continue;
-            }
-
-            // TODO allow hexadecimal output
-            if (fwrite(buffer, sizeof(char), current_req_bytes, out) != current_req_bytes) {
-                LOG(ERROR, "Failed to write random data.\n");
-                err = 1;
-                break;
-            }
-
-            bytes_read += current_req_bytes;
+        if ((err = get_random(&dev, buffer, current_req_bytes))) {
+            LOG(ERROR, "Failed to get random data.\n");
+            // It might be useful to wait when the disk fails to provide random data
+            // (e.g., to regain enough entropy).
+            // Sleeping should be cut-off after some number of tries.
+            // sleep(5);
+            continue;
         }
+
+        // TODO allow hexadecimal output
+        // TODO decide on using fopen or open solely
+        if (fwrite(buffer, sizeof(char), current_req_bytes, out) != current_req_bytes) {
+            LOG(ERROR, "Failed to write random data.\n");
+            err = 1;
+            break;
+        }
+
+        bytes_read += current_req_bytes;
     }
 
     fclose(out);
